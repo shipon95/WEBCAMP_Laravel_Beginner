6d1dd6c3a9bba498949e8c6fc74f7fe0042fe625
Your Name <you@example.com>
2023-10-10 12:52:01 +0000

    ページネーションを書いた

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -14,14 +14,18 @@ class TaskController extends Controller
      *
      * @return \Illuminate\View\View
      */
-    public function list()
+      public function list()
     {
+        // 1Page辺りの表示アイテム数を設定
+        $per_page = 2;
+
         // 一覧の取得
         $list = TaskModel::where('user_id', Auth::id())
                          ->orderBy('priority', 'DESC')
                          ->orderBy('period')
                          ->orderBy('created_at')
-                         ->get();
+                         ->paginate($per_page);
+                        // ->get();
 /*
 $sql = TaskModel::where('user_id', Auth::id())
                  ->orderBy('priority', 'DESC')
